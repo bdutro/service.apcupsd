388b569075084a3f0405b273e410b9b5ef945dc2
Brett Dutro <brett.dutro@gmail.com>
2019-03-31 11:56:27 -0500

    - Update paths to point to correct locations of scripts/conf files - addon.py will automatically create required directories/files

--- addon.py
+++ addon.py
@@ -3,12 +3,15 @@ import xbmc
 import xbmcaddon
 import subprocess
 import os
+import shutil
 
 __addon__ = xbmcaddon.Addon(id='service.apcupsd')
 __addondir__ = xbmc.translatePath(__addon__.getAddonInfo('path'))
 __userdir__ = xbmc.translatePath(__addon__.getAddonInfo('profile'))
 APCUPSD_BIN_PATH = os.path.join(__addondir__, 'resources/lib/apcupsd/sbin/apcupsd')
 APCUPSD_CONF_PATH = os.path.join(__userdir__, 'apcupsd.conf')
+LOCKFILE_PATH = '/run/apcupsd'
+APCUPSD_EXAMPLE_CONF_PATH = os.path.join(__addondir__, 'resources/lib/apcupsd/etc/apcupsd.conf')
 
 class ApcupsdInstance(object):
     def __init__(self, bin_path, conf_path):
@@ -18,6 +21,10 @@ class ApcupsdInstance(object):
         self.pid = None
 
     def start(self):
+        if not os.path.exists(LOCKFILE_PATH):
+            os.makedirs(LOCKFILE_PATH)
+        if not os.path.exists(APCUPSD_CONF_PATH):
+            shutil.copyfile(APCUPSD_EXAMPLE_CONF_PATH, APCUPSD_CONF_PATH)
         if not self.started:
             self.pid = subprocess.Popen([self.bin_path, '-f', self.conf_path])
 
